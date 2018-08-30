001b98722fbf3981b27229e3e54d58e12f130eab
Mark Olson <mlo@baskerville.org>
2018-08-30 11:18:21 -0400

    Remove FanacNames from project

--- Helpers.py
+++ Helpers.py
@@ -1,6 +1,5 @@
 import os
 from bs4 import NavigableString
-import FanacNames
 import urllib
 from datetime import datetime
 import math
@@ -214,45 +213,6 @@ def CreateFanacOrgAbsolutePath(fanacDir, str):
 def FormatLink(name, url):
     # TODO: Do we need to deal with tgurning blanks into %20 whatsits?
     return '<a href='+url+'>'+name+'</a>'
-
-
-#==================================================================================
-# Compare a filename, volume and number with a second set
-def CompareIssueSpec(name1, vol1, num1, whole1, name2, vol2, num2, whole2):
-    if not FanacNames.FanacNames().CompareNames(name1, name2):
-        return False
-
-    # Sometimes we pass in integers and sometimes strings.  Let's work with strings here
-    if vol1 is not None:
-        vol1=str(vol1)
-    if num1 is not None:
-        num1=str(num1)
-    if vol2 is not None:
-        vol2=str(vol2)
-    if num2 is not None:
-        num2=str(num2)
-    if whole1 is not None:
-        whole1=str(whole1)
-    if whole2 is not None:
-        whole2=str(whole2)
-
-    # The strategy is to allow over-specification (e.g., both Vol+Num and WholeNum.
-    # So one or the other must match
-    if vol1 != None and num1 != None and vol1 == vol2 and num1 == num2:
-        return True
-    if whole1 != None and whole1 == whole2:
-        return True
-
-    # A Onesie should have all the numbers None or empty string
-    if (vol1 is None or vol1 == "")\
-            and (vol2 is None or vol2 == "")\
-            and (num1 is None or num1 == "")\
-            and (num2 is None or num2 == "")\
-            and (whole1 is None or whole1 == "")\
-            and (whole2 is None or whole2 == ""):
-        return True
-
-    return False
 
 
 #==================================================================================
