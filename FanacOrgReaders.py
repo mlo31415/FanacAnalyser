7a78a436ee0a3a9818ab64aa43fd314c4c2c2ee0
mlo31415 <mlo@baskerville.org>
2025-11-02 16:59:03 -0500

    When doing a FIP with AlphabetizeIndividually=True, the series name is the issue name

--- FanacOrgReaders.py
+++ FanacOrgReaders.py
@@ -208,7 +208,7 @@ def ReadFanacFanzineIndexPageNew(fanzineName: str, directoryUrl: str, html: str)
         # Add the tags and the series info pointer
         for fii in fiiList:
             # Create a special series just for this issue.
-            fii.Series=FanzineSeriesInfo(SeriesName=DropTrailingSequenceNumber(fii.IssueName), DirURL=directoryUrl, Issuecount=1, Pagecount=0, Editor=fii.Editor, Country=country, AlphabetizeIndividually=True, Keywords=kwds)
+            fii.Series=FanzineSeriesInfo(SeriesName=fii.IssueName, DirURL=directoryUrl, Issuecount=1, Pagecount=0, Editor=fii.Editor, Country=country, AlphabetizeIndividually=True, Keywords=kwds)
             if isnewszines:
                 fii.Taglist.append("newszine")
     else:
