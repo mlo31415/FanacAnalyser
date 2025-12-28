9403cde4d09491644260803dd1983cebbf7b5848
mlo31415 <mlo@baskerville.org>
2025-12-27 19:16:27 -0500

    Dedal with a problem of some characters -- Cyrillic, I think -- not being handled correctly.

--- FanacAnalyser.py
+++ FanacAnalyser.py
@@ -624,7 +624,7 @@ def main():
     # Files are created in reports/APAs
     mailingsCSVFile=Settings().Get("mailings csv file", "mailings.csv")
 
-    with open(os.path.join(rootDir, mailingsCSVFile), 'w', newline="") as csvfile:
+    with open(os.path.join(rootDir, mailingsCSVFile), 'w', newline="", encoding="utf-8") as csvfile:
         filewriter=csv.writer(csvfile, delimiter=',', quotechar='"', escapechar=r'£', quoting=csv.QUOTE_MINIMAL)
 
         columnheaders=["IssueName", "Series", "SeriesName", "DisplayName", "DirURL", "PageName", "FIS", "Locale", "PageCount", "Editor", "TagList", "Mailings"]
