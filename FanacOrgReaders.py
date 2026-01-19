2e6a117e54448f33dd764010d10fd4ebd191867d
mlo31415 <mlo@baskerville.org>
2026-01-19 12:55:58 -0500

    Remove unused import

--- FanacOrgReaders.py
+++ FanacOrgReaders.py
@@ -12,7 +12,7 @@ from Locale import Locale
 from Settings import Settings
 
 from Log import Log, LogSetHeader, LogError
-from HelpersPackage import ReadList, FindBracketedText, ParseFirstStringBracketedText, ExtractHTMLUsingFanacStartEndCommentPair, MessageBox
+from HelpersPackage import ReadList, FindBracketedText, ParseFirstStringBracketedText, ExtractHTMLUsingFanacStartEndCommentPair
 from HelpersPackage import ExtractBetweenHTMLComments, RemoveHyperlink
 from HelpersPackage import CanonicizeColumnHeaders
 from HelpersPackage import ExtractInvisibleTextInsideFanacComment
